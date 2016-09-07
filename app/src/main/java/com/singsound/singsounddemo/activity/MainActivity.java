06af5ff713a018fdcf9db94d2daf89857921575f
syc <1142403657@qq.com>
2016-09-07 12:03:56 +0800

    2  test update  Mactivity

--- app/src/main/java/com/singsound/singsounddemo/activity/MainActivity.java
+++ app/src/main/java/com/singsound/singsounddemo/activity/MainActivity.java
@@ -1,29 +1,22 @@
 package com.singsound.singsounddemo.activity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
-import com.singsound.singsounddemo.Config;
-import com.singsound.singsounddemo.GridViewAdapter;
 import com.singsound.singsounddemo.R;
 import com.singsound.singsounddemo.fragment.CloudFragment;
 import com.singsound.singsounddemo.fragment.NativeFragment;
+import com.singsound.singsounddemo.utils.TitleBar;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
 
@@ -33,7 +26,34 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        initTitle();
         initUI();
+    }
+
+    private void initTitle() {
+        final TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
+        titleBar.setBackgroundColor(Color.parseColor("#30809f"));
+        titleBar.setLeftTextColor(Color.WHITE);
+        titleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        titleBar.setTitle("先声教育语音评测演示");
+        titleBar.setTitleColor(Color.WHITE);
+        titleBar.setSubTitleColor(Color.WHITE);
+        titleBar.setDividerColor(Color.GRAY);
+
+        titleBar.setActionTextColor(Color.WHITE);
+        titleBar.addAction(new TitleBar.TextAction("关于") {
+            @Override
+            public void performAction(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 
     private void initUI() {
@@ -42,8 +62,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
         bottomNavigationBar
                 .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC
                 );
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, "cloud").setActiveColorResource(R.color.orange))
-                .addItem(new BottomNavigationItem(R.mipmap.ic_launcher, "native").setActiveColorResource(R.color.orange))
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.online_unselected, getResources().getString(R.string.online)).setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.mipmap.offine_unselected, getResources().getString(R.string.offine)).setActiveColorResource(R.color.orange))
                 .setFirstSelectedPosition(0)
                 .initialise();
         bottomNavigationBar.setTabSelectedListener(this);
@@ -57,8 +77,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
     private void setDefaultFragment() {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.layFrame,cloudFragment);
-        transaction.add(R.id.layFrame,nativeFragment);
+        transaction.add(R.id.layFrame, cloudFragment);
+        transaction.add(R.id.layFrame, nativeFragment);
         transaction.show(cloudFragment);
         transaction.hide(nativeFragment);
         transaction.commit();
@@ -109,6 +129,5 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
 
     @Override
     public void onTabReselected(int position) {
-
     }
 }
