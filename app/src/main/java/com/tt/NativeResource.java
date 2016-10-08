b0d5b8033265a0ad4362e05b6959516019813515
syc <1142403657@qq.com>
2016-10-08 10:18:39 +0800

    sdk  test

--- app/src/main/java/com/tt/NativeResource.java
+++ app/src/main/java/com/tt/NativeResource.java
@@ -5,6 +5,7 @@ public class NativeResource {
     public static String vadResourceName = "vad.0.1.bin";
     public static String zipResourceName = "resource.zip";
 
+
     public static String native_zip_res_path = "{"
             + "\"en.sent.score\":{\"res\": \"%s/eval/bin/eng.snt.pydnn.16bit\"}"
             + ",\"en.word.score\":{\"res\": \"%s/eval/bin/eng.wrd.pydnn.16bit\"}"
